0c9f7f4d14c8771c4f27588c26cc742c31cfb708
Vaishnavi Balasaheb Ghumare <120745949+VGHUMARE1@users.noreply.github.com>
2025-04-30 10:47:13 +0530

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, required: true, unique: true },
+  phoneNumber: { type: String, required: true },
   password: { type: String, required: true },
   photo: { type: String, required: true }
 });
